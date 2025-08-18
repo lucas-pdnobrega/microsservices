d4155362bc9855c19cf1255672256184392691d8
Lucas Nobrega <105940701+lucas-pdnobrega@users.noreply.github.com>
2025-08-18 20:21:52 -0300

    feat: timeout and retry attempts (practice 4)

--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -2,11 +2,15 @@ package payment_adapter
 
 import (
 	"context"
-
+	"log"
+	"time"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"github.com/lucas-pdnobrega/microservices-proto/golang/payment"
 	"github.com/lucas-pdnobrega/microservices/order/internal/application/core/domain"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Adapter struct {
@@ -15,7 +19,15 @@ type Adapter struct {
 
 func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(
+		opts,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(
+			grpc_retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+			grpc_retry.WithMax(5),
+			grpc_retry.WithBackoff(grpc_retry.BackoffLinear(2*time.Second)),
+		)),
+	)
 	conn, err := grpc.Dial(paymentServiceUrl, opts...)
 	if err != nil {
 		return nil, err
@@ -26,11 +38,21 @@ func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 }
 
 func (a *Adapter) Charge(order *domain.Order) error {
-	_, err := a.payment.Create(context.Background(), &payment.
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	_, err := a.payment.Create(ctx, &payment.
 		CreatePaymentRequest{
-		UserId:     order.CustomerId,
+		UserId:     order.CustomerID,
 		OrderId:    order.ID,
 		TotalPrice: order.TotalPrice(),
 	})
+
+	if err != nil {
+		if st, ok := status.FromError(err); ok && st.Code() == codes.DeadlineExceeded {
+			log.Printf("Timeout: deadline exceeded for Payment.Charge for order %d", order.ID)
+		}
+	}
+
 	return err
 }
